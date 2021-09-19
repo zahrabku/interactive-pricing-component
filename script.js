cf7b8106b805d5c49393e24e81d611635ca5481a
zahrabku <zahrabku>
2021-09-19 12:13:24 +0430

    init

---
+++ script.js
@@ -0,0 +1,8 @@
+var slider = document.querySelector("slider");
+var output = document.querySelector("amount");
+output.innerHTML = slider.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+slider.oninput = function() {
+  output.innerHTML = this.value;
+}
